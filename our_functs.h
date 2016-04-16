2073e70e5daf0f20de6e620a0a5332b9c16b625c
Rouvem <rouvem.p@gmail.com>
2016-04-15 22:38:40 -0500

    Update our_functs.h
    Modification of word finding algorithm inside word_search function. Should work correctly now. If you want capitalization to matter, simply respond with 'y' or 'Y'. If not, respond with 'n' or 'N', and your inputted word, along with all words in text will be lowercased! Note the heavy placement of the cookie_monster() function. The monster has to constantly eat up the stdin cookies! Make a function in the future that will call scanf or getchar, and then call cookie_monster() from it, that way you have cleaner code.

--- our_functs.h
+++ our_functs.h
@@ -9,22 +9,26 @@
 #include <stdbool.h>
 #define MYMAX 2500
 
- //function that inputs into a string AND gets rid of \n
+ //functions that gets rid of \n
  void cookie_monster(void);
- int word_search(char *word, bool capital, int word_len);
+ void new_line();
+
+ // header for word_search
+ int word_search(char *file_name, char *word, bool capital, int word_len);
 
 int my_input_func(char array[])
 {
-	int success = 1; //assume succesful
+	int success = 1; //assume successful
 	int len = 0;
-	char *var;
-	var = fgets(array, MYMAX, stdin);
-	if(var == NULL){
+	int var;
+	scanf("%s", array);
+	//printf("var: %d\narray: %s\n", var, array);
+	if(var == 0){
 		success = 0;
 	}
 
 	len = strlen(array);
-	if((array[len -1]) == '\n'){
+	if((array[len - 1]) == '\n'){
 		array[len - 1] = '\0'; //terminate string
 		len -= 1;
 	}else{
@@ -42,32 +46,31 @@ int my_input_func(char array[])
  }
 
  //end of function that accepts input
-int word_search(char *word,  bool capital, int word_len)
+int word_search(char *file_name, char *word,  bool capital, int word_len)
 {
 	int count = 0;
-    FILE *fp;
     register int i;
     int j;
-    fp = fopen("test.txt", "r");
+	FILE *fp;
+    fp = fopen(file_name, "r");
     if(fp == NULL){
         perror(fp);
     }
     char reader;
     char c_array[word_len];
 
-	if(capital){
+	if(!capital){
 		for(j = 0; j < word_len; ++j){
 	    		if(toupper(word[j])){
         			word[j] = tolower(word[j]);
-        			break;
 			}//if
 		}
 	}
-    printf("%s\n", word);
-    do{
-        reader = fscanf(fp, "%s", c_array);
+	printf("word: %s\n", word);
+    while(fscanf(fp, "%s", c_array) == 1){
 
-	    if(capital){
+        printf("%s\n", c_array);
+	    if(!capital){
        		for(i = 0; i < strlen(c_array); ++i){
         		if(toupper(c_array[i])){
         			c_array[i] = tolower(c_array[i]);
@@ -77,12 +80,75 @@ int word_search(char *word,  bool capital, int word_len)
         if(strcmp(word, c_array) == 0){
            count += 1;
         }
-    }while(reader != EOF);
-
+    }
     fclose(fp);
     return count;
 }
 
 
+void find_word_home(char *file_name, int file_name_len)
+{
+	char searched_word[250];
+	char ch;
+	int num;
+	int valid;
+	char che;
 
+	do{
+      		printf("Enter the word that you are searching for:\n");
+    		valid = my_input_func(searched_word);
+    		do{
+                	printf("Do you care about capitalization?\n");
+        	 	ch = getchar();
+                	if(ch == 'Y' || ch == 'y'){
+                    		system("cls");
+                    		num = word_search(file_name, searched_word, 1, strlen(searched_word));
+                		printf("%d words\n", num);
+                    		cookie_monster();
+                	}
+                	else if(ch == 'N' || ch == 'n'){
+                    		system("cls");
+                    		num = word_search(file_name, searched_word, 0, strlen(searched_word));
+                    		printf("%d words\n", num);
+                    		cookie_monster();
+                	}
+                	else{
+                    		cookie_monster();
+                	}
+    		}while(ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n');
+  			int main = 0;
+  			while(!main){
+                		printf("Return to main menu, Y/N?\n");
+                		scanf("%c", &che);
+                		cookie_monster();
+                		if(che == 'y' || che == 'Y'){
+                    			main = 1;
+                		}
+                		else if(che == 'N' || che == 'n'){
+                    			break;
+                		}
+                		else{
+                		}
+  			}
+            	if(main){
+                	break;
+            	}
 
+ 	}while(valid == 1);
+
+}
+
+void user_interface(void)
+{
+	printf("Enter...\n");
+	printf("Digit (1) to find a searched phrase in the Bible!\n");
+	printf("Digit (0) to quit!\n");
+}
+
+void new_line()
+{
+    char symbol;
+    do{
+        scanf("%c", &symbol);
+    }while(symbol != '\n');
+}
